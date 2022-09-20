db78ac7174abb599e98b36d92b67af0dd8011ed2
Evgenii <evg4755@yandex.rugit config --global user.name evgeniieglobal user.email evg4755@yandex.ru>
2022-09-20 17:58:02 +0300

    задача10

---
+++ задача10/Program.cs
@@ -0,0 +1,20 @@
+﻿int number = ReadInt("Введите трехзначное число: ");
+int amount = number.ToString().Length;
+if (amount < 3 || amount > 3)
+{
+    Console.WriteLine("Не трехзначаное число");
+}
+else
+{
+    Console.WriteLine(InCenter(number));
+}
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int InCenter(int a)
+{ 
+    int result = ((a / 10) % 10);
+    return result;
+}
